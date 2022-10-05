4bfe2d8440303fb1182ab80d56aedce05e39cd7b
Rafael Lemos <rhplemos@gmail.com>
2022-10-04 22:13:52 -0300

    feat: API integration

---
+++ app/src/main/java/com/rafaellemos/callmewhere/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rafaellemos.callmewhere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+    }
+}
